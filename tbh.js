7cdd1aa1280d2fae4ce485c415f6d5d54901c998
kurayami-kun <114032565+kurayami-kun@users.noreply.github.com>
2022-10-21 23:35:08 +0100

    Add files via upload

--- tbh.js
+++ tbh.js
@@ -1,13 +1,6 @@
 let password = prompt("Enter the password");
-if(password=="yagami light"){
-    window.alert("Welcome Kurayami-Kun");
-}
 while(password!="yagami light"){
     window.alert("Wrong password, try again");
-    document.write("<!--");
     password = prompt("Enter The Password");
-    if( password == "yagami light" ){
-        document.write("-->");
-        window.alert("Welcome Kurayami-Kun");
-    }
 }
+window.alert("Welcome Kurayami-Kun");
